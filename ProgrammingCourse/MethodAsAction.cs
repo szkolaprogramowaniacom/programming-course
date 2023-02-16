416f961f7be8e9b7d074b53df0c721e7cc03aaf1
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-16 22:57:23 +0100

    Methods as params using Action

---
+++ ProgrammingCourse/MethodAsAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    // simple example
+    //var actions = new MethodAsAction();
+    //actions.Calculate(3, 2, actions.Add);
+    //actions.Calculate(4, 2, actions.Subtract);
+
+    internal class MethodAsAction
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"a+b={a + b}");
+        }
+
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine($"a-b={a - b}");
+        }
+
+        public void Calculate(int a, int b, Action<int, int> operation)
+        {
+            operation(a, b);
+        }
+
+    }
+}
