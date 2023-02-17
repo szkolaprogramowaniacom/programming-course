291284a3e68a7965b58ad15975a69bf8321e01c9
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-17 21:03:16 +0100

    Local methods

---
+++ ProgrammingCourse/LocalMethod.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class LocalMethod
+{
+    // easy example
+    //var objectWithLocalMethods = new LocalMethod();
+    //objectWithLocalMethods.Run();
+
+    public LocalMethod()
+    {
+    }
+
+    public void Run()
+    {
+        Step1();
+        Step2();
+
+        void Step1()
+        {
+            Console.WriteLine("Step 1");
+        }
+
+        void Step2()
+        {
+            Console.WriteLine("Step 2");
+        }
+    }
+}
