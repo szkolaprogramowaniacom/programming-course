d00a688cc52790dad085d337cf448c63d739e2d4
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-22 11:00:19 +0100

    Classes VS Tuples

---
+++ ProgrammingCourse/ClassVsTuple.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProgrammingCourse
+{
+    //var tupleTester = new ClassVsTuple();
+    //var personalInfo = tupleTester.GetPersonalInformation();
+    //Console.WriteLine($"{personalInfo.Name} {personalInfo.Age}");
+
+    //var(name, age) = tupleTester.GetPersonalInformationUsingTuple();
+    //Console.WriteLine($"{name} {age}");
+
+    public class PersonalInformation
+    {
+       public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class ClassVsTuple
+    {
+        public PersonalInformation GetPersonalInformation()
+        {
+            return new PersonalInformation
+            {
+                Name = "John Smith",
+                Age = 22
+            };
+        }
+
+        public (string, int) GetPersonalInformationUsingTuple()
+        {
+            return ("Ann Smith", 21);
+        }
+    }
+}
