74f666b1e8fa20adadcc4d87537222e76f417005
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-24 20:36:53 +0100

    Enums using order example

---
+++ ProgrammingCourse/Order/OrderStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Order
+{
+
+//    var orders = new List<Order>()
+//    {
+//        new Order(){
+//            Id=1,
+//            Name = "TV",
+//            Type = "RTV",
+//            Status= OrderStatus.Waiting
+//        },
+//        new Order(){
+//            Id=2,
+//            Name="Mouse",
+//            Type="COMPUTERS",
+//            Status= OrderStatus.InProgres
+//        }
+//    };      
+//    // find orders with type = computers
+//    var computers = orders.Where(c => c.Type == "COMPUTERS").ToList();
+//    Console.WriteLine(computers.Count);      
+//    var inProgres = orders.Where(c => c.Status == OrderStatus.InProgres).ToList();
+//    Console.WriteLine(inProgres.Count);
+
+    public enum OrderStatus
+    {
+        Waiting,
+        InProgres,
+        Sent,
+        Delivered,
+        Rejected
+    }
+}
