bd8648b3a639124636247327ab09cf29e1aaaf7e
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-26 20:29:58 +0100

    Access Modifiers

---
+++ ProgrammingCourse/AccessModifiers/CalculatedOutput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.AccessModifiers
+{
+//    var calculatedOutput = new CalculatedOutput();
+//    calculatedOutput.Input = 2;
+//    calculatedOutput.CalculateOutput();
+//    //calculatedOutput.Output = 4;
+//    var result = calculatedOutput.Output;
+//    Console.WriteLine(result);
+
+    // internal
+    // public
+    // private
+    // protected
+    // internal protected
+    public class CalculatedOutput
+    {
+        public double Input { get; set; }
+        public double Output { get; private set; }
+
+        public void CalculateOutput()
+        {
+            Output = Input * 2;
+        }
+    }
+}
