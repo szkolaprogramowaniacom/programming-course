aa9df08d941a3ba6400a2039baf17e6ffa4f6f18
Fluffy Dev <info@szkolaprogramowania.com>
2023-02-27 15:42:28 +0100

    Object Initialization

---
+++ ProgrammingCourse/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    // using constructor with params
+    //var john = new Person(1, "John");
+    //john.Name = "John";
+    //john.Id = 1;
+
+    // using constructor without params
+    //var anonymous = new Person();
+
+    //var monica = new Person {Id=2, Name = "Monica" };
+
+    internal class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public required int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
