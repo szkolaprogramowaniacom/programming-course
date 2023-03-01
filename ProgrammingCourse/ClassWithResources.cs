389dadc825d773e3a5e2642275debd66076c8238
Fluffy Dev <info@szkolaprogramowania.com>
2023-03-01 08:44:23 +0100

    IDisposable - resources release

---
+++ ProgrammingCourse/ClassWithResources.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    //if (true)
+    //{
+    //    using var res = new ClassWithResources();
+    //    // end
+    //}
+
+    //using (var res = new ClassWithResources())
+    //{
+
+    //}
+
+    public class ClassWithResources : IDisposable
+    {
+        public ClassWithResources()
+        {
+            Console.WriteLine("Opening file connections");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Closing file connections");
+        }
+    }
+}
