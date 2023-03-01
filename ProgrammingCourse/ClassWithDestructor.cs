ee743328a60c9e0e060829820f81726da18abab9
Fluffy Dev <info@szkolaprogramowania.com>
2023-03-01 18:02:16 +0100

    Destructors

---
+++ ProgrammingCourse/ClassWithDestructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    //if (true)
+    //{
+    //    var obj = new ClassWithDestructor();
+    //}
+
+    //Console.WriteLine("end");
+    
+    public class ClassWithDestructor
+    {
+        public ClassWithDestructor()
+        {
+            Console.WriteLine("Created");
+        }
+
+        ~ClassWithDestructor()
+        {
+            Console.WriteLine("Object destructed");
+        }
+    }
+}
