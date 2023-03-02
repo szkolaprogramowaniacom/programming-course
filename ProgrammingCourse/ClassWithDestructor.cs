70498ffac3cca840c4b79c1b3bca1a10e34468f5
Fluffy Dev <info@szkolaprogramowania.com>
2023-03-02 21:36:39 +0100

    Updates

--- ProgrammingCourse/ClassWithDestructor.cs
+++ ProgrammingCourse/ClassWithDestructor.cs
@@ -1,23 +1,32 @@
-﻿using System;
+﻿using ProgrammingCourse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProgrammingCourse
-{
-    //if (true)
-    //{
-    //    var obj = new ClassWithDestructor();
-    //}
+//for (int i = 0; i < 100; i++)
+//{
+//    var obj = new ClassWithDestructor();
+//}
 
-    //Console.WriteLine("end");
-    
+//if (true)
+//{
+//    var obj = new ClassWithDestructor();
+//}
+
+//Console.WriteLine("end");
+
+namespace ProgrammingCourse
+{  
     public class ClassWithDestructor
     {
+        public string Name { get; set; }
+
         public ClassWithDestructor()
         {
-            Console.WriteLine("Created");
+            Name = RandomStringGenerator.GenerateString(10000);
+            Console.WriteLine("Object Created");
         }
 
         ~ClassWithDestructor()
