7870139dd5e82d187b8872d3c4a4eb133989c9f2
Fluffy Dev <info@szkolaprogramowania.com>
2023-03-24 13:19:03 +0100

    Inheritance

---
+++ ProgrammingCourse/OOP/Inharitance/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.OOP.Inharitance
+{
+    internal class Car
+    {
+        public Car(string color)
+        {
+            Color = color;
+        }
+
+        public string Color { get; private set; }
+        
+        public void Run()
+        {
+            Console.WriteLine("Running...");
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("Opening...");
+        }
+    }
+}
---
+++ ProgrammingCourse/OOP/Inharitance/Ranault.cs
@@ -0,0 +1,33 @@
+﻿using ProgrammingCourse.OOP.Inharitance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Car car = new Car("RED");
+//car.Open();
+//car.Run();
+
+//Ranault renault = new Ranault("WHITE");
+//renault.Open();
+//renault.Run();
+//renault.ConnectWithCompany();
+
+//Car r1 = new Ranault("BLACK");
+
+namespace ProgrammingCourse.OOP.Inharitance
+{
+    internal class Ranault : Car
+    {
+        public Ranault(string color) : base(color)
+        {
+            
+        }
+
+        public void ConnectWithCompany()
+        {
+            Console.WriteLine("Connecting");
+        }
+    }
+}
