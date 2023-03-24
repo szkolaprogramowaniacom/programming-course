3dbcc4b3fe765e8f9353f4a9667e63b0282b1f23
Fluffy Dev <info@szkolaprogramowania.com>
2023-03-24 12:59:04 +0100

    Properties

---
+++ ProgrammingCourse/OOP/ClassWithProperties.cs
@@ -0,0 +1,64 @@
+﻿using ProgrammingCourse.OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//var classWithProperties = new ClassWithProperties("Smith");
+//classWithProperties.FirstName = "John";
+////classWithProperties.FirstName = "";
+////classWithProperties.LastName = "";
+////classWithProperties.Age = 12;
+//Console.WriteLine(classWithProperties.Name);
+
+namespace ProgrammingCourse.OOP
+{
+    internal class ClassWithProperties
+    {
+        public ClassWithProperties(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { 
+                if(value == "")
+                {
+                    throw new ArgumentException("Wrong value");
+                }
+                firstName = value; 
+            }
+        }
+        
+        private string lastName;
+        public string LastName { 
+            get 
+            { 
+                return lastName;
+            }
+        }
+
+        public int Age { get; } = 12;
+
+        public string Name
+        {
+            get => $"{firstName} {lastName}";
+        }
+
+        private int size;
+        public int Size
+        {
+            get => CalculateSize();
+            set => size = value;
+        }
+
+        private int CalculateSize()
+        {
+            return 232;
+        }
+    }
+}
