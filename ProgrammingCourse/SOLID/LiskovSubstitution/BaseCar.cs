2eec79c4c804e4d8515cb15c0b42e22ae62dcdce
Fluffy Dev <info@szkolaprogramowania.com>
2023-04-17 11:49:09 +0200

    Liskov Substitution

---
+++ ProgrammingCourse/SOLID/LiskovSubstitution/BaseCar.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingCourse.SOLID.LiskovSubstitution
+{
+    internal abstract class BaseCar
+    {
+        public int Speed { get; set; }
+        public virtual void Accelerate()
+        {
+            Speed += 10;
+        }
+    }
+}
---
+++ ProgrammingCourse/SOLID/LiskovSubstitution/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.SOLID.LiskovSubstitution
+{
+    internal abstract class Car : BaseCar
+    {
+        public Car(int speedLimit)
+        {
+            SpeedLimit = speedLimit;
+        }        
+        
+        public int SpeedLimit { get; }
+
+        public abstract void Break();
+        public abstract int GetSpeed();
+    }
+}
