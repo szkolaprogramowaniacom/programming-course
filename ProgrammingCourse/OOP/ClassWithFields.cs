65314d275d975d876b413bf154b29d42fd1bff09
Fluffy Dev <info@szkolaprogramowania.com>
2023-05-24 14:27:18 +0200

    fields

---
+++ ProgrammingCourse/OOP/ClassWithFields.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.OOP
+{
+    internal class ClassWithFields
+    {        
+        private string name;
+
+        public void InitializeName(string name)
+        {
+            
+            this.name = name;
+        }
+
+        public const string DEFAULT_TYPE = "BOOK11";
+
+        //public readonly string DefaultName = "BK_NM1";        
+        //public static readonly string DefaultValue = "123";
+
+        public void Run()
+        {    
+            var type = "book";
+            Calculate(type);
+            CalculateOther(type);
+
+            name = "John" + DEFAULT_TYPE;
+            TestName();
+            Console.WriteLine($"{name} running");
+        }
+
+        private void TestName()
+        {
+            if (name.Equals("John", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("OK");
+            }
+        }
+
+        private void Calculate(string type)
+        {
+            // use type
+            Console.WriteLine(type);
+        }
+
+        private void CalculateOther(string type)
+        {
+            // use type
+            Console.WriteLine(type);
+        }
+
+        public int Size { get; set; }
+    }
+}
--- ProgrammingCourse/Program.cs
+++ ProgrammingCourse/Program.cs
@@ -1,17 +1,31 @@
 ﻿
 using ProgrammingCourse;
+using ProgrammingCourse.OOP;
 
 Console.WriteLine("""
-╔════════════════════════════════════════════════════╗
-║ .-.     .-.                                        ║   
-║ |M|   .-|P|---.                                    ║
-║ |S|---| |Y|===|   LEARN HOW TO TALK TO MACHINES    ║
-║ |.|   |J|T| C |                                    ║
-║ |N| C |A|H|===|     .NET Programming Course        ║
-║ |E| + |V|O| # |  https://szkolaprogramowania.com   ║
-║ |T| + |A|N|===|                                    ║
-║ |_|___|_|_|___|                                    ║
-╚════════════════════════════════════════════════════╝
-
+╔═════════════════════════════════════╗
+║ .-.     .-.                         ║   
+║ |M|   .-|P|---.                     ║
+║ |S|---| |Y|===|  LEARN HOW TO TALK  ║
+║ |.|   |J|T| C |     TO MACHINES     ║
+║ |N| C |A|H|===|                     ║
+║ |E| + |V|O| # |  .NET Programming   ║
+║ |T| + |A|N|===|                     ║
+║ |_|___|_|_|___|                     ║
+╠═════════════════════════════════════╣
+║       szkolaprogramowania.com       ║
+╚═════════════════════════════════════╝
 """);
 
+void ShowParams(params int[] values)
+{
+	foreach (var value in values)
+	{
+		Console.Write($"{value} ");		
+	}
+    Console.WriteLine();
+}
+
+ShowParams(new int[] { 1, 2, 3 });
+ShowParams(1, 3, 2);
+ShowParams(1, 3, 2, 5, 4, 3, 4);
