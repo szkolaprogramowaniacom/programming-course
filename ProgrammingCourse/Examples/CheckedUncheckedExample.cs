6283b8148a4fc95a626a25451a9f411c70bc5520
Fluffy Dev <info@szkolaprogramowania.com>
2023-06-07 14:50:38 +0200

    checked and unchecked

---
+++ ProgrammingCourse/Examples/CheckedUncheckedExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class CheckedUncheckedExample
+    {
+        public void Run()
+        {
+            checked
+            {
+                int a = int.MaxValue;
+                int b = a + 1;
+                Console.WriteLine(b);
+                unchecked
+                {
+                    // unchecked code
+                }
+            }
+        }
+    }
+}
