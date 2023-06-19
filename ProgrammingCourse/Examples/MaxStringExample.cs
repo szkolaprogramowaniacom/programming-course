c07de071204320701f5866b2524d54bf1cb38701
Fluffy Dev <info@szkolaprogramowania.com>
2023-06-19 21:18:01 +0200

    MaxString

---
+++ ProgrammingCourse/Examples/MaxStringExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class MaxStringExample
+    {
+        public void Run()
+        {
+            var text = "";
+            var add = "lsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsdlsjdflkjsd";
+
+            while (true)
+            {
+                text += add;
+
+                if (text.Length % 100000 == 0)
+                {
+                    Console.WriteLine(text.Length);
+                }
+            }
+        }
+    }
+}
