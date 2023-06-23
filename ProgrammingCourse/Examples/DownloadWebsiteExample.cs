863f86852385033ab10b00cb63ce6787ab030470
Fluffy Dev <info@szkolaprogramowania.com>
2023-06-23 18:19:02 +0200

    Download website

---
+++ ProgrammingCourse/Examples/DownloadWebsiteExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class DownloadWebsiteExample
+    {
+        public async void RunAsync()
+        {
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync("https://google.com");
+            File.WriteAllText(@"C:\Projekty\SzkolaProgramowania.com\Pliki\google.txt", content);
+        }
+    }
+}
