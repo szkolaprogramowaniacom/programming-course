56ddadd17d5027cbf7898cf05618d8dd3c30a9cc
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-10 14:53:45 +0200

    Switch with when

---
+++ ProgrammingCourse/Examples/SwitchExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class SwitchExample
+    {
+        public void Run()
+        {
+            var size = 88;
+
+            switch (size)
+            {
+                case var mySize when mySize > 100:
+                    Console.WriteLine("LARGE");
+                    break;
+                case var mySize when mySize <= 100:
+                    Console.WriteLine("SMALL");
+                    break;
+                default:
+                    break;
+            }
+
+            //if (size > 100)
+            //{
+            //    Console.WriteLine("LARGE");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("SMALL");
+            //}
+        }
+    }
+}
