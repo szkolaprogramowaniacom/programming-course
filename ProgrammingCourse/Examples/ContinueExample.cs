c705bd1090938a2a8b0f201c63deca5d433538a5
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-18 16:20:15 +0200

    continue keyword

---
+++ ProgrammingCourse/Examples/ContinueExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class ContinueExample
+    {
+        public void Run()
+        {
+            var allowed = new List<char>
+            {
+                'a',
+                'd',
+                'f'
+            };
+
+            var content = "skdkfdkjakkkekkwkdkkflkkrkldkltkiikdslasklsdkf";
+
+            foreach (var letter in content)
+            {
+                if (!allowed.Contains(letter)) continue;
+                Console.Write(letter);
+            }
+        }
+    }
+}
