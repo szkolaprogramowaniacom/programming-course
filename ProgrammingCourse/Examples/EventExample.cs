1e2be5cae76454243932becb7b530ef71b029a1d
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-19 19:07:43 +0200

    Event example

---
+++ ProgrammingCourse/Examples/EventExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class EventExample
+    {
+        public void Run()
+        {
+            var timer = new System.Timers.Timer(3000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+            {
+                Console.WriteLine("Event Elapsed");
+            }
+
+            Console.WriteLine("Press any key");
+            Console.Read();
+        }
+    }
+}
