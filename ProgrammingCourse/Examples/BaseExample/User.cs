e35b3de85c8d27bcf8ad5c7aaa150700b53aa655
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-24 14:13:48 +0200

    implicit explicite conversions

--- ProgrammingCourse/Examples/BaseExample/User.cs
+++ ProgrammingCourse/Examples/BaseExample/User.cs
@@ -8,13 +8,25 @@ namespace ProgrammingCourse.Examples.BaseExample
 {
     internal class User
     {
+        private readonly string username;
+
         public User(string username)
         {
-            
+            this.username = username;
         }
         public string GetInfo()
         {
             return "This is user";
+        }
+
+        public static explicit operator User(string text)
+        {
+            return new User(text);
+        }
+
+        public static implicit operator string(User user)
+        {
+            return user.username;
         }
 
     }
---
+++ ProgrammingCourse/Examples/ImplicitExplicitExample.cs
@@ -0,0 +1,21 @@
+﻿using ProgrammingCourse.Examples.BaseExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class ImplicitExplicitExample
+    {
+        public void Run()
+        {
+            var user = new User("John Smith");
+            string text = user;
+            Console.WriteLine(text);
+
+            User user2 = (User)"John Smith";
+        }
+    }
+}
