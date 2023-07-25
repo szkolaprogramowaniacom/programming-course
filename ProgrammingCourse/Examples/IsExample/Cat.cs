bf1f4b07eea8b98a8e838b558e3bb2c38de35a29
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-25 17:47:56 +0200

    is keyword

---
+++ ProgrammingCourse/Examples/IsExample/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples.IsExample
+{
+    //int a = 12;
+    //object obj = a;
+
+    //void DoSth(object obj)
+    //{
+    //    if(obj is int)
+    //    {
+    //        Console.WriteLine("This is int");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Something else");
+    //    }
+    //}
+
+    //DoSth("asd");
+
+    //Animal animal = new Cat();
+
+    //void MakeSound(Animal animal)
+    //{
+    //    animal.MakeSound();
+    //}
+
+    //MakeSound(animal);
+
+    internal class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meaow");
+        }
+    }
+}
