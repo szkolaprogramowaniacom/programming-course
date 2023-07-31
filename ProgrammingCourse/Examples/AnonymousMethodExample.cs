a97463556dd9744e3ac68e621dad940b69c235fb
Fluffy Dev <info@szkolaprogramowania.com>
2023-07-31 19:08:52 +0200

    Anonymous methods

---
+++ ProgrammingCourse/Examples/AnonymousMethodExample.cs
@@ -0,0 +1,41 @@
+﻿namespace ProgrammingCourse.Examples
+{
+    internal class AnonymousMethodExample
+    {
+        void LoadItems()
+        {
+            // loading
+            Console.WriteLine("Loading items");
+            Task.Delay(5000).Wait();
+            Console.WriteLine("OK");
+        }
+
+        public void Run()
+        {
+            //Action method1 = LoadItems;
+            //method1();
+            var method2 = (int a) =>
+            {
+                // loading
+                Console.WriteLine("Loading items");
+                Task.Delay(5000).Wait();
+                Console.WriteLine("OK");
+            };
+
+            var items = new List<string> { "asd", "dss" };
+
+            bool IsA(string text)
+            {
+                if (text.Contains('a'))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            var firstWithA = items.First((text) => text.Contains('a'));
+        }
+    }
+}
