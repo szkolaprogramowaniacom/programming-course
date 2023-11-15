5773093adb887337f22fe9fca4d22bbcb84402d6
Fluffy Dev <info@szkolaprogramowania.com>
2023-11-15 15:20:48 +0100

    Collection multi remove

---
+++ ProgrammingCourse/Examples/CollectionExample.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class CollectionExample
+    {
+        public record Product(int Id, string Name, string Category);
+
+        public void Run()
+        {
+            var products = new List<Product>
+            {
+                new Product(1, "TV", "AGD"),
+                new Product(2, "Coffee Machine", "AGD"),
+                new Product(3, "Phone", "Phones")
+            };
+
+            // wrong way
+            //foreach (var product in products)
+            //{
+            //    if(product.Category == "Phones")
+            //    {
+            //        products.Remove(product);
+            //    }
+            //}
+
+            // long code and enumeration twice
+            //var productsToRemove = new List<Product>();
+            //foreach (var product in products)
+            //{
+            //    if (product.Category == "Phones")
+            //    {
+            //        productsToRemove.Add(product);
+            //    }
+            //}
+            //products.RemoveAll(product => productsToRemove.Contains(product));
+
+            products.RemoveAll(product => product.Category == "Phones");
+
+            // what predicate is
+            //products.RemoveAll(WhereProductIsPhone);
+
+            // show collection after operation
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+        public bool WhereProductIsPhone(Product product)
+        {
+            return product.Category == "Phones";
+        }
+        
+    }
+}
