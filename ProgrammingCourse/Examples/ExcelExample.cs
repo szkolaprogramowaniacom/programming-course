ea41161882f4cad3734bcb1ade9bfc9628995858
Fluffy Dev <info@szkolaprogramowania.com>
2023-11-20 17:34:09 +0100

    Excel use example

---
+++ ProgrammingCourse/Examples/ExcelExample.cs
@@ -0,0 +1,27 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class ExcelExample
+    {
+        public void Run()
+        {
+            var folder = @"C:\Projekty\SzkolaProgramowania.com\Pliki";
+
+            // excell operations
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("This is a test");
+            worksheet.Cell(1, 1).Value = "John";
+            worksheet.Column("A").AdjustToContents();
+
+            // save to file
+            var filePath = Path.Combine(folder, "Created Excel.xlsx");
+            workbook.SaveAs(filePath);
+        }
+    }
+}
