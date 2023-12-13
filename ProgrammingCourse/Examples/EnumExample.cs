624eee570d2c9e372515bc667b5782cd85fd5886
Fluffy Dev <info@szkolaprogramowania.com>
2023-12-13 20:00:01 +0100

    Enum

---
+++ ProgrammingCourse/Examples/EnumExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class EnumExample
+    {
+        public void RunExample()
+        {
+            //AddProduct("Samsung", "Phone");
+            AddProduct("Samsung", ProductType.Phone);
+        }
+
+        void AddProduct(string name, ProductType type)
+        {
+            Console.WriteLine($"Product: {name}, type: {type}");
+        }
+    }
+
+    enum ProductType
+    {
+        Phone,
+        TV
+    }
+}
