782fae12462c25228c7213d07dccf15fc2d0d39a
Fluffy Dev <info@szkolaprogramowania.com>
2023-12-21 14:59:19 +0100

    Round, Floor, Ceiling

---
+++ ProgrammingCourse/Examples/MathExamples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class MathExamples
+    {
+        public void RoundExample()
+        {
+            var price = 12.32;
+
+            var rounded = Math.Round(price);
+            var floor = Math.Floor(price);
+            var ceiling = Math.Ceiling(price);
+
+            Console.WriteLine($"Round: {rounded}, Floor: {floor}, Ceiling: {ceiling}");
+        }
+    }
+}
--- ProgrammingCourse/Program.cs
+++ ProgrammingCourse/Program.cs
@@ -20,3 +20,4 @@ Console.WriteLine("""
 ║       szkolaprogramowania.com       ║
 ╚═════════════════════════════════════╝
 """);
+
