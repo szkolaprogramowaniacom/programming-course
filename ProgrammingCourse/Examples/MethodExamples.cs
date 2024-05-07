3d4af26ff32a4fa789bc5b44ade9c211af5ccfac
Fluffy Dev <info@szkolaprogramowania.com>
2024-05-07 16:51:08 +0200

    Methods parameters example

---
+++ ProgrammingCourse/Examples/MethodExamples.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgrammingCourse.Examples
+{
+    internal class MethodExamples
+    {
+        record Product(string Name, string Category);
+
+        public void Run()
+        {
+            AddProduct("Samsung Galaxy S23", "Phones");
+
+            var product = new Product("Samsung Galaxy S21", "Phones");
+            AddProductModel(product);
+
+            //AddProductModel(new Product("Samsung Galaxy S6", "Phones"));
+        }
+
+        void AddProduct(string name, string category)
+        {
+            // updating DB
+            Console.WriteLine($"Product {name} added...");
+        }
+
+        void AddProductModel(Product product)
+        {
+            // updating DB
+            Console.WriteLine($"Product {product.Name} added...");
+        }
+    }
+}
