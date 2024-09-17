181926ab9fa674319246d408eab64196706c1439
Fluffy Dev <info@szkolaprogramowania.com>
2024-09-17 15:20:40 +0100

    Operators == and !=

---
+++ ProgrammingCourse/Examples/OperatorsExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class OperatorsExample
+    {
+        public void Run()
+        {
+            EqualExample();
+        }
+
+        private void EqualExample()
+        {
+            bool isOk = false;
+            int size = 12;
+
+            // == porównanie
+            // != różne
+            if (size != 22)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+        }
+    }
+}
