ff85c52b8e26f197b2b4bf1f162c66a84f8be2fd
Fluffy Dev <info@szkolaprogramowania.com>
2024-11-27 17:28:49 +0000

    Passing Methods

---
+++ ProgrammingCourse/Examples/PassingMethodsExample.cs
@@ -0,0 +1,40 @@
+﻿using ProgrammingCourse.Examples.Classes.CalculatorExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse.Examples
+{
+    internal class PassingMethodsExample
+    {
+        public void Run()
+        {
+            //var size = 12;
+            //void ShowSize(int size)
+            //{
+            //    Console.WriteLine(size);
+            //}
+
+            Calculate(2, 3, Add);
+            Calculate(2, 3, Subtract);
+        }
+
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        void Calculate(int a, int b, Func<int, int, int> operation)
+        {
+            var result = operation(a, b);
+            Console.WriteLine($"operation({a},{b}) = {result}");
+        }
+    }
+}
