db87628cc3fe85b2123fe8a7c65ce8415aff8550
Fluffy Dev <info@szkolaprogramowania.com>
2024-12-03 19:37:30 +0000

    Date time

--- ProgrammingCourse/Examples/DateTimeExamples.cs
+++ ProgrammingCourse/Examples/DateTimeExamples.cs
@@ -19,5 +19,15 @@ namespace ProgrammingCourse.Examples
             var later2 = date.Add(span);
             Console.WriteLine(later2);
         }
+
+        public void TimeSpanExample()
+        {
+            var eventDate = new DateTime(2024, 12, 24);
+            var now = DateTime.Now;
+            TimeSpan time = eventDate - now;
+            Console.WriteLine($"{time.TotalDays:0.00} days to event: {eventDate.ToShortDateString()}");
+            Console.WriteLine($"{time.TotalHours:0.00} hours to event: {eventDate.ToShortDateString()}");
+            Console.WriteLine($"{time.TotalMinutes:0.00} minutes to event: {eventDate.ToShortDateString()}");
+        }
     }
 }
